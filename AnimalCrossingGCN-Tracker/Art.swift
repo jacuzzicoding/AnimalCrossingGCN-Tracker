1e815025c6484b63244c1a62ae747c0f9052e9c7
Brock Jenkinson <brockjenkinson@gmail.com>
2024-10-11 02:04:39 -0400

    Add Art struct template and ArtDetailView for painting tracking functionality based on my bug.swift code. Will add remaining paintings next.

--- AnimalCrossingGCN-Tracker/Art.swift
+++ AnimalCrossingGCN-Tracker/Art.swift
@@ -5,20 +5,54 @@
 //  Created by Brock Jenkinson on 10/11/24.
 //
 
-struct Art: Identifiable {
-    let id = UUID()
-    let name: String
-    let real: Bool?
-    var donated: Bool
-    var obtainedDate: Date?
+import Foundation
+import SwiftData
+import SwiftUI
+
+// This function returns the default array of art pieces
+func getDefaultArt() -> [Art] {
+    return [
+        Art(name: "Academic Painting", isDonated: false),
+        Art(name: "Amazing Painting", isDonated: false),
+        Art(name: "Basic Painting", isDonated: false),
+        Art(name: "Calm Painting", isDonated: false),
+        // Add more art pieces here...
+    ]
 }
 
-extension Art {
-    static func allArtPieces() -> [Art] {
-        return [
-            Art(name: "Academic Painting", real: nil, donated: false),
-            Art(name: "Amazing Painting", real: nil, donated: false),
-            // Add all art pieces from the game
-        ]
+@Model
+class Art: ObservableObject, Identifiable {
+    var id: UUID
+    var name: String
+    var isDonated: Bool
+
+    init(name: String, isDonated: Bool = false) {
+        self.id = UUID()
+        self.name = name
+        self.isDonated = isDonated
+    }
+}
+
+struct ArtDetailView: View {
+    var art: Art
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(art.name)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Toggle("Donated", isOn: Binding(
+                get: { art.isDonated },
+                set: { newValue in
+                    art.isDonated = newValue
+                }
+            ))
+            .padding(.top)
+
+            Spacer()
+        }
+        .padding()
+        .navigationTitle(art.name)
     }
 }
