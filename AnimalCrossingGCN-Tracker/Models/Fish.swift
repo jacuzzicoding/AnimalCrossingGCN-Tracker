eea8e504f3d06fbe876529556fd8f3db9930423e
Brock Jenkinson <brockjenkinson@gmail.com>
2024-12-12 01:54:54 -0500

    refractor: reformat fish vector & add games attribute to Fish model instances for compatibility with ACGame enum

--- AnimalCrossingGCN-Tracker/Models/Fish.swift
+++ AnimalCrossingGCN-Tracker/Models/Fish.swift
@@ -65,45 +65,285 @@ struct FishDetailView: View {
 
 func getDefaultFish() -> [Fish] {
     return [
-        Fish(name: "Sea Bass", season: "All", location: "Sea", isDonated: false),
-        Fish(name: "Brook Trout", season: "All", location: "Lake", isDonated: false),
-        Fish(name: "Crucian Carp", season: "All", location: "River", isDonated: false),
-        Fish(name: "Carp", season: "All", location: "River", isDonated: false),
-        Fish(name: "Koi", season: "All", location: "River", isDonated: false),
-        Fish(name: "Barbel Steed", season: "All", location: "River", isDonated: false),
-        Fish(name: "Dace", season: "All", location: "River", isDonated: false),
-        Fish(name: "Catfish", season: "June - September", location: "River", isDonated: false),
-        Fish(name: "Giant Catfish", season: "July - August", location: "Lake", isDonated: false),
-        Fish(name: "Pale Chub", season: "All", location: "River", isDonated: false),
-        Fish(name: "Bitterling", season: "November - February", location: "River", isDonated: false),
-        Fish(name: "Loach", season: "March - May", location: "River", isDonated: false),
-        Fish(name: "Bluegill", season: "All", location: "River", isDonated: false),
-        Fish(name: "Small Bass", season: "All", location: "River", isDonated: false),
-        Fish(name: "Bass", season: "All", location: "River", isDonated: false),
-        Fish(name: "Large Bass", season: "All", location: "River", isDonated: false),
-        Fish(name: "Giant Snakehead", season: "July - August", location: "Lake", isDonated: false),
-        Fish(name: "Eel", season: "September - October", location: "River", isDonated: false),
-        Fish(name: "Freshwater Goby", season: "All", location: "River", isDonated: false),
-        Fish(name: "Pond Smelt", season: "December - February", location: "River", isDonated: false),
-        Fish(name: "Sweetfish", season: "July - September", location: "River", isDonated: false),
-        Fish(name: "Cherry Salmon", season: "March - June, September - November", location: "River", isDonated: false),
-        Fish(name: "Rainbow Trout", season: "March - June", location: "River", isDonated: false),
-        Fish(name: "Stringfish", season: "December - February", location: "River", isDonated: false),
-        Fish(name: "Salmon", season: "September", location: "River (mouth)", isDonated: false),
-        Fish(name: "Goldfish", season: "All", location: "River", isDonated: false),
-        Fish(name: "Pop-eyed Goldfish", season: "All", location: "River", isDonated: false),
-        Fish(name: "Guppy", season: "April - November", location: "River", isDonated: false),
-        Fish(name: "Angelfish", season: "May - October", location: "River", isDonated: false),
-        Fish(name: "Piranha", season: "June - September", location: "River", isDonated: false),
-        Fish(name: "Arowana", season: "June - September", location: "River", isDonated: false),
-        Fish(name: "Coelacanth", season: "All (raining)", location: "Sea", isDonated: false),
-        Fish(name: "Crawfish", season: "April - September", location: "Pond", isDonated: false),
-        Fish(name: "Frog", season: "May - August", location: "Pond", isDonated: false),
-        Fish(name: "Killifish", season: "April - August", location: "River", isDonated: false),
-        Fish(name: "Jellyfish", season: "August", location: "Sea", isDonated: false),
-        Fish(name: "Red Snapper", season: "All", location: "Sea", isDonated: false),
-        Fish(name: "Barred Knifejaw", season: "March - November", location: "Sea", isDonated: false),
-        Fish(name: "Arapaima", season: "July - September", location: "River", isDonated: false),
-        Fish(name: "Squid", season: "December - August", location: "Sea", isDonated: false)
+        Fish(
+            name: "Sea Bass",
+            season: "All",
+            location: "Sea",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Brook Trout",
+            season: "All",
+            location: "Lake",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Crucian Carp",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Carp",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Koi",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Barbel Steed",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Dace",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Catfish",
+            season: "June - September",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Giant Catfish",
+            season: "July - August",
+            location: "Lake",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Pale Chub",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Bitterling",
+            season: "November - February",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Loach",
+            season: "March - May",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Bluegill",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Small Bass",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Bass",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Large Bass",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Giant Snakehead",
+            season: "July - August",
+            location: "Lake",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Eel",
+            season: "September - October",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Freshwater Goby",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Pond Smelt",
+            season: "December - February",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Sweetfish",
+            season: "July - September",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Cherry Salmon",
+            season: "March - June, September - November",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Rainbow Trout",
+            season: "March - June",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Stringfish",
+            season: "December - February",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Salmon",
+            season: "September",
+            location: "River (mouth)",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Goldfish",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Pop-eyed Goldfish",
+            season: "All",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Guppy",
+            season: "April - November",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Angelfish",
+            season: "May - October",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Piranha",
+            season: "June - September",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Arowana",
+            season: "June - September",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Coelacanth",
+            season: "All (raining)",
+            location: "Sea",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Crawfish",
+            season: "April - September",
+            location: "Pond",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Frog",
+            season: "May - August",
+            location: "Pond",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Killifish",
+            season: "April - August",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Jellyfish",
+            season: "August",
+            location: "Sea",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Red Snapper",
+            season: "All",
+            location: "Sea",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Barred Knifejaw",
+            season: "March - November",
+            location: "Sea",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Arapaima",
+            season: "July - September",
+            location: "River",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        ),
+        Fish(
+            name: "Squid",
+            season: "December - August",
+            location: "Sea",
+            isDonated: false,
+            games: [ACGame.gameCube.rawValue]
+        )
     ]
 }
