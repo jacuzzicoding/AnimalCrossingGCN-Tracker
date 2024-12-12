194797ea1437f3decf2c1106920016f53ba6147a
Brock Jenkinson <brockjenkinson@gmail.com>
2024-12-12 02:29:23 -0500

    refactor: format Fossil vector for improved readability (vertical and not horizontal now)

--- AnimalCrossingGCN-Tracker/Models/Fossil.swift
+++ AnimalCrossingGCN-Tracker/Models/Fossil.swift
@@ -49,30 +49,150 @@ struct FossilDetailView: View {
 
 func getDefaultFossils() -> [Fossil] {
     return [
-        Fossil(name: "T. Rex", part: "Skull", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "T. Rex", part: "Torso", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "T. Rex", part: "Tail", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Triceratops", part: "Skull", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Triceratops", part: "Torso", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Triceratops", part: "Tail", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Stegosaurus", part: "Skull", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Stegosaurus", part: "Torso", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Stegosaurus", part: "Tail", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Pteranodon", part: "Skull", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Pteranodon", part: "Left Wing", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Pteranodon", part: "Right Wing", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Plesiosaurus", part: "Skull", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Plesiosaurus", part: "Neck", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Plesiosaurus", part: "Torso", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Apatosaurus", part: "Skull", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Apatosaurus", part: "Torso", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Apatosaurus", part: "Tail", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Mammoth", part: "Skull", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Mammoth", part: "Torso", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Amber", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Ammonite", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Dinosaur Egg", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Dinosaur Track", isDonated: false, games: [.ACGCN]),
-        Fossil(name: "Trilobite", isDonated: false, games: [.ACGCN])
+        Fossil(
+            name: "T. Rex",
+            part: "Skull",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "T. Rex",
+            part: "Torso",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "T. Rex",
+            part: "Tail",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Triceratops",
+            part: "Skull", 
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Triceratops",
+            part: "Torso",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Triceratops",
+            part: "Tail",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Stegosaurus",
+            part: "Skull",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Stegosaurus",
+            part: "Torso",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Stegosaurus",
+            part: "Tail",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Pteranodon",
+            part: "Skull",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Pteranodon",
+            part: "Left Wing",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Pteranodon",
+            part: "Right Wing",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Plesiosaurus",
+            part: "Skull",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Plesiosaurus",
+            part: "Neck",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Plesiosaurus",
+            part: "Torso",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Apatosaurus",
+            part: "Skull",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Apatosaurus",
+            part: "Torso",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Apatosaurus",
+            part: "Tail",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Mammoth",
+            part: "Skull",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Mammoth",
+            part: "Torso",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Amber",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Ammonite",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Dinosaur Egg",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Dinosaur Track",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Fossil(
+            name: "Trilobite",
+            isDonated: false,
+            games: [.ACGCN]
+        )
     ]
 }
