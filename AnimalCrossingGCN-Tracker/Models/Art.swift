68d8b800734dc7aff7d2fe594a9a34d30081f582
Brock Jenkinson <brockjenkinson@gmail.com>
2024-12-12 02:22:37 -0500

    refactor: clean up Art vector to match the new format in Fish.swift

--- AnimalCrossingGCN-Tracker/Models/Art.swift
+++ AnimalCrossingGCN-Tracker/Models/Art.swift
@@ -16,7 +16,7 @@ class Art {
     var basedOn: String
     //var imageName: String // this will store the name of the image file
     var isDonated: Bool
-    var games: [ACGame] // match Fish.swift implementation exactly
+    var games: [ACGame]
 
     init(name: String, basedOn: String, isDonated: Bool = false, games: [ACGame]) {
         self.id = UUID()
@@ -27,6 +27,7 @@ class Art {
         self.games = games
     }
 }
+
 struct ArtDetailView: View {
     var art: Art
 
@@ -61,18 +62,83 @@ struct ArtDetailView: View {
 
 func getDefaultArt() -> [Art] {
     return [
-        Art(name: "Academic Painting", basedOn: "Vitruvian Man by Leonardo da Vinci", isDonated: false, games: [.ACGCN]),
-        Art(name: "Amazing Painting", basedOn: "The Night Watch by Rembrandt", isDonated: false, games: [.ACGCN]),
-        Art(name: "Basic Painting", basedOn: "The Blue Boy by Thomas Gainsborough", isDonated: false, games: [.ACGCN]),
-        Art(name: "Calm Painting", basedOn: "A Sunday Afternoon on the Island of La Grande Jatte by Georges Seurat", isDonated: false, games: [.ACGCN]),
-        Art(name: "Classic Painting", basedOn: "Washington Crossing the Delaware by Emanuel Leutze", isDonated: false, games: [.ACGCN]),
-        Art(name: "Common Painting", basedOn: "The Gleaners by Jean-François Millet", isDonated: false, games: [.ACGCN]),
-        Art(name: "Dainty Painting", basedOn: "The Star (Dancer on Stage) by Edgar Degas", isDonated: false, games: [.ACGCN]),
-        Art(name: "Famous Painting", basedOn: "Mona Lisa by Leonardo da Vinci", isDonated: false, games: [.ACGCN]),
-        Art(name: "Flowery Painting", basedOn: "Sunflowers by Vincent van Gogh", isDonated: false, games: [.ACGCN]),
-        Art(name: "Moving Painting", basedOn: "The Birth of Venus by Sandro Botticelli", isDonated: false, games: [.ACGCN]),
-        Art(name: "Quaint Painting", basedOn: "The Milkmaid by Johannes Vermeer", isDonated: false, games: [.ACGCN]),
-        Art(name: "Scary Painting", basedOn: "Otani Oniji II by Toshusai Sharaku", isDonated: false, games: [.ACGCN]),
-        Art(name: "Worthy Painting", basedOn: "Liberty Leading the People by Eugène Delacroix", isDonated: false, games: [.ACGCN])
+        Art(
+            name: "Academic Painting",
+            basedOn: "Vitruvian Man by Leonardo da Vinci",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Amazing Painting",
+            basedOn: "The Night Watch by Rembrandt",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Basic Painting",
+            basedOn: "The Blue Boy by Thomas Gainsborough",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Calm Painting",
+            basedOn: "A Sunday Afternoon on the Island of La Grande Jatte by Georges Seurat",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Classic Painting",
+            basedOn: "Washington Crossing the Delaware by Emanuel Leutze",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Common Painting",
+            basedOn: "The Gleaners by Jean-François Millet",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Dainty Painting",
+            basedOn: "The Star (Dancer on Stage) by Edgar Degas",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Famous Painting",
+            basedOn: "Mona Lisa by Leonardo da Vinci",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Flowery Painting",
+            basedOn: "Sunflowers by Vincent van Gogh",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Moving Painting",
+            basedOn: "The Birth of Venus by Sandro Botticelli",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Quaint Painting",
+            basedOn: "The Milkmaid by Johannes Vermeer",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Scary Painting",
+            basedOn: "Otani Oniji II by Toshusai Sharaku",
+            isDonated: false,
+            games: [.ACGCN]
+        ),
+        Art(
+            name: "Worthy Painting",
+            basedOn: "Liberty Leading the People by Eugène Delacroix",
+            isDonated: false,
+            games: [.ACGCN]
+        )
     ]
 }
